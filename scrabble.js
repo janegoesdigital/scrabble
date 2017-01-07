7485f29c4d57a58f0ca09c06494e2b6b1bb72e02
jadarling <janeodarling@gmail.com>
2017-01-08 03:08:20 +1100

    Added styling including scrabble tiles

--- scrabble.js
+++ scrabble.js
@@ -5,13 +5,43 @@ $( document ).ready(function() {
 console.log("jQuery ready");
 
 
-var wordsIn = ["a","b","c", "do", "don't", "orange", "ox", "eleven", "qi"];
-var wordsOut = ["f", "g", "bbbl", "goad"];
+var wordsIn = [
+		"ae", "ti", "wo", "re", "er", "eh", "ay", "qr", "qt", "qi", "oy", "oi", "te", "ad", "io", "xi", "ta", "oz", "zo", "zee", "zoa", "zoon", "ala", "ikon", "la", "da", "mo", "zo", "ca", "ol", "ed", "ai", "da", "ut", "oh", "yo","pa", "si", "ho", "og","oy", "oi", "go", "ye", "pdf", "pdl", "pd", "ph", "pi", "pc","po", "ope", "qat", "qtr", "qts", "qid", "qiyas", "qua", "iwi(s)"
+		];
+
+var wordsOut = [
+	"ig", "ir", "ih", "ew", "aw", "ow", "uw", "eg", "ee", "et", "fe", "ag", "eb", "ja", "aa", "le", "eq", "fi", "qy", "el", "ep", "ap", "iw", "ue", "uf", "ro", "za", "ze", "zi", "zu", "al", "il", "ey", "ug", "na", "ne", "zu", "zi", "ed", "ga", "ge", "gi", "gu", "un", "ef", "pe", "pu", "py", "ve"
+	];
+
+//List all the wordsOut
+var listEach = function(testList, appendTo){
+	var list = testList.sort();
+	// console.log(list);
+	for ( i = 0; i < list.length; i ++) {
+		var eachWord = list[i];
+		// console.log(eachWord);
+		var $newP = $( "<p class='tile'></p>" );
+		$newP.html(eachWord);
+		$(appendTo).append($newP);
+		}
+	};
+
+
+listEach(wordsIn, "#wordsIn");
+
+listEach(wordsOut, "#wordsOut");
+
+
+
+
+
 
 //find me words with the testLetter in it so I can play it
 
 var hasLetter = function(testLetter){
 	var selectedWords = [];
+	var sortList = selectedWords.sort();
+	console.log("The sorted list is: " + sortList);
 	var message;
 		for (i  = 0 ; i < wordsIn.length; i ++) {
 			var testWord = wordsIn[i];
@@ -21,19 +51,23 @@ var hasLetter = function(testLetter){
 			}
 		}
 		if ( selectedWords.length > 0 ) {
-			message = "The words that contain the letter <strong>" + testLetter + "</strong> are: " + selectedWords;
-			console.log(message);
-		}
+				$(".oldLetter").remove();
+				for ( i = 0; i < sortList.length; i ++) {
+					var eachWord = sortList[i];
+					var $newP = $( "<p class='tile oldLetter'></p>" );
+					$newP.html(eachWord);
+					$("#hasLetterAnswer").append($newP);
+				}
+			}
 		else {
-		message = "Hmmm...can't find any words with the letter" +  testLetter + "- can you belive that!";
+		message = "<div class='oldLetter'> Hmmm...can't find any words with the letter " +  testLetter + "- can you belive that! </div>";
+
+			$("#oldLetter").remove();
+		 	var $newerP = $( "<p id='tile oldLetter'></p>" );
+		 	$newerP.html(message);
+	 	$("#hasLetterAnswer").append($newerP);
 		}
 		console.log(message);
-
-		$("#oldLetter").remove();
-		var $newP = $( "<p id='oldLetter'></p>" );
-		$newP.html(message);
-		$("#hasLetterAnswer").prepend($newP);
-
 	};
 
 
@@ -61,14 +95,13 @@ var isWord = function(word){
 	var checkIn = findWord(word, wordsIn);
 	console.log("Checkin " + checkIn);
 	var checkOut = findWord(word, wordsOut);
-
 	console.log("Checkout: " + checkOut);
 	var message;
 	if (checkIn === true ){
-		message = "Huzzah! Yes, the word " + "<strong>" + word + "</strong>" + " is in our list and you can use it!";
+		message = "<div class='tile'>" + word + "</div> <br>" + "Huzzah! Yes, it's in our list and you can use it!";
 	}
 	else if (checkOut === true) {
-		message = "Nah.  We looked " + "<strong>" + word + "</strong>" + " up before and it ain't a word";
+		message = "Nah.  We looked " + "<strong> " + word + "</strong>" + " up before and it ain't a word";
 	}
 	else {
 		message = "Oh no! We don't know yet whether " + "<strong>" + word + "</strong> " + " is in or out!";
--- test.js
+++
@@ -1,4 +0,0 @@
-var wordsIn = ["a","b","c", "do", "don't", "orange", "ox", "eleven", "qi"];
-var wordsOut = ["f", "g", "bbbl", "goad"];
-
-//this works to look through a string and see if there is a letter in it
